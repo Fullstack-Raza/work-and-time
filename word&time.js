028e4c53faf73af364d59604add78534e7360b13
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-07-26 21:54:11 +0300

    time&work

---
+++ word&time.js
@@ -0,0 +1,16 @@
+// 1.work --> total amount of work a single person or a group needs todo .
+
+//  2.time --> total amount of time a group  or a person take to do that work.
+
+// 3. efficiency --> the rate at which a person or a group can do that certain task is efficiency.
+
+//  how to solve 
+
+////////////////////////////
+//  now to solve work and time questions wo can  use 2 concepts those are Fraction method and LCM   method
+
+//  fraction method 
+// a can do a piece of work in 10 days whereas b can do same work in 20 days if both of them work toghether how many dayes will it take 
+
+//  lcm method 
+// // a can do a piece of work in 10 days whereas b can do same work in 20 days if both of them work toghether how many dayes will it take 
